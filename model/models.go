b5e3921ccfd62b31f03b23c282d9d5d001e047b1
EgeBykstn <ege.buyukustun@ozu.edu.tr>
2022-08-29 11:49:48 +0300

    Auto increment ID and filter fields added

--- model/models.go
+++ model/models.go
@@ -1,13 +1,35 @@
 package model
 
+import (
+	"sync"
+	"time"
+)
+
 type Product struct {
-	Id       int    `json:"id"`
-	Code     string `json:"code"`
-	Name     string `json:"name"`
-	Category string `json:"category"`
-	Price    int    `json:"price"`
-	Color    string `json:"color"`
-	Size     int    `json:"size"`
+	Id        int       `json:"id"`
+	Code      string    `json:"code"`
+	Name      string    `json:"name"`
+	Category  string    `json:"category"`
+	Price     int       `json:"price"`
+	Color     string    `json:"color"`
+	Size      int       `json:"size"`
+	UpdatedAt time.Time `json:"-"`
+	CreatedAt time.Time
 }
-type Migrate struct {
+type AutoInc struct {
+	sync.Mutex
+	id int
+}
+type Filter struct {
+	Code string `form:"code" json:"code,omitempty"`
+	Name string `form:"name" json:"name,omitempty"`
+}
+
+func (s *AutoInc) ID() (id int) {
+	s.Lock()
+	defer s.Unlock()
+
+	id = s.id
+	s.id++
+	return id
 }
