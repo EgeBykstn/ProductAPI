b195fd401db3ab40338bc7f570cd8a602738ba9d
EgeBykstn <ege.buyukustun@ozu.edu.tr>
2022-08-29 11:48:39 +0300

    migration fixed

--- database/db.go
+++ database/db.go
@@ -12,16 +12,23 @@ var (
 )
 
 func NewDB() *gorm.DB {
+	//migration := config.Migrate{}
 	dbURL := "postgres://user:pass@localhost:5432/crud"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	//	DB.Migrator().AddColumn(model.Product{}, "CreatedAt")
+	//	DB.Migrator().AddColumn(model.Product{}, "UpdatedAt")
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	DB.AutoMigrate(&model.Product{})
-
+	if DB.Migrator().HasTable(model.Product{}) == false {
+		err := DB.AutoMigrate(&model.Product{})
+		if err != nil {
+			panic("migration failed")
+		}
+	}
 	return DB
 }
 func GetDBInstance() *gorm.DB {
