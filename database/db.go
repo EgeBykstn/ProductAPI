6c7723a0826c49876268120eeb710c65ff946604
EgeBykstn <ege.buyukustun@ozu.edu.tr>
2022-09-13 14:23:47 +0300

    migration product table added

--- database/db.go
+++ database/db.go
@@ -4,25 +4,47 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"product-api/config"
+	"product-api/model"
 	"time"
 )
 
 var (
 	DB *gorm.DB
 )
+
 var Loc, _ = time.LoadLocation("Europe/Minsk")
 
-func NewDB() *gorm.DB {
-	dbURL := "postgres://user:pass@database:5432/crud"
-	var err error
-	DB, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+type GormDB interface {
+	Find(dest interface{}, conds ...interface{}) (tx *gorm.DB)
+	First(dest interface{}, conds ...interface{}) (tx *gorm.DB)
+	Where(query interface{}, args ...interface{}) (tx *gorm.DB)
+	Create(value interface{}) (tx *gorm.DB)
+	Model(value interface{}) (tx *gorm.DB)
+	Updates(values interface{}) (tx *gorm.DB)
+	Delete(value interface{}, conds ...interface{}) (tx *gorm.DB)
+}
 
+func NewDB() *gorm.DB {
+	conString := config.GetPostgresConnectionString()
+	DB, err := gorm.Open(postgres.Open(conString), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	return DB
-}
-func GetDBInstance() *gorm.DB {
 
 	return DB
+}
+
+func GetDBInstance() *gorm.DB {
+	return DB
+}
+
+func MigrateProductTable(DB *gorm.DB) {
+	c := config.Config{}
+	if c.IsMigrate == false {
+		err := DB.AutoMigrate(&model.Product{})
+		if err != nil {
+			panic("migration failed")
+		}
+	}
 }
